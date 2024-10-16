2f701b23897e42b8bcdd254ceaf96b74e9d03d9a
Naichoy / Dexmi <61109859+Dexmi@users.noreply.github.com>
2024-10-16 05:58:02 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -24,10 +24,10 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
+set DIRNAME=%~Dexmi
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set APP_HOME=%Dexmi%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
