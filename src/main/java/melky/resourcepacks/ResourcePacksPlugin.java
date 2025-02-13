d2a2880b1e6e3ae681e7d2c944c270e4c20248fa
Ron Young <ron@ryoung.io>
2025-02-13 14:05:53 -0600

    add deposit_lock boxes

--- src/main/java/melky/resourcepacks/ResourcePacksPlugin.java
+++ src/main/java/melky/resourcepacks/ResourcePacksPlugin.java
@@ -1,7 +1,6 @@
 package melky.resourcepacks;
 
 import com.google.inject.Provides;
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.nio.file.Path;
 import java.util.concurrent.ScheduledExecutorService;
@@ -19,8 +18,6 @@ import net.runelite.api.GameState;
 import net.runelite.api.events.BeforeRender;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ScriptPostFired;
-import net.runelite.api.events.ScriptPreFired;
-import net.runelite.api.widgets.WidgetUtil;
 import net.runelite.client.RuneLite;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.chat.ChatColorType;
@@ -39,7 +36,6 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.interfacestyles.Skin;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
-import net.runelite.client.util.ColorUtil;
 import net.runelite.client.util.ImageUtil;
 import okhttp3.HttpUrl;
 
@@ -309,13 +305,8 @@ public class ResourcePacksPlugin extends Plugin
 	@Subscribe
 	public void onScriptPostFired(ScriptPostFired event)
 	{
-		if (event.getScriptId() == 85) {
-			log.debug("widget {}", ColorUtil.toHexColor(new Color(client.getScriptActiveWidget().getTextColor())));
-		}
-
 		if (!overrides.isEmpty() && overrides.contains(event.getScriptId()))
 		{
-
 			for (var widgetOverride : overrides.get(event.getScriptId()))
 			{
 				resourcePacksManager.addPropertyToWidget(widgetOverride, false);
